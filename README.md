95f1fec2671f7717fe8aea598d93bf5dd1f1d7b1
tkleinloog <tkleinloog@gmail.com>
2021-11-11 19:52:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# angular-tour-of-heroes
+ Angular tutorial tour of heroes
---
+++ src/app/hero.ts
@@ -0,0 +1,6 @@
+export interface Hero{
+    id: number;
+    name: string;
+}
+
+// https://angular.io/tutorial/toh-pt4 part 4: services
